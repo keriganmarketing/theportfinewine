56a48612daff66ac9a353863db6240c4360e396b
Robert Crites <robertcritesiii@gmail.com>
2018-08-23 12:30:12 -0500

    template changes

--- public/themes/wordplate/views/pages/front.blade.php
+++ public/themes/wordplate/views/pages/front.blade.php
@@ -9,40 +9,59 @@
         <main role="main" class="front">
             <div class="mast">
                 <img src="themes/wordplate/assets/images/portfinewinetop.png" alt="wine bottles" style="width: 100%;">
+                <div class="text-center">
+                    <div class="centered">
+                        <h1 style="font-size:8vw;">Discover</h1>
+                        <p style="font-size:1vw;">Port Fine Wine & Spirits offers the widest selection of wines, imported</p>
+                        <p style="font-size:1vw;">& cold beers, cigars and liquor from Mexico Beach to Cape San Blas.</p>
+                    </div>
+                </div>
             </div>
+
 
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6 col-lg-4 text-black box-container">
+                    <div class="col-md-6">
                         <img src="themes/wordplate/assets/images/phoneimage.png" alt="assorted liquors" style="width: 90%;">
                     </div>
-                    <div class="col-md-6 col-lg-4 text-black box-container">
+                    <div class="text-left">
                         <h1>Cheers!</h1>
-                            <p>This is where the content will go.</p>
+                        <p>This is where the content will go.</p>
                     </div>
                 </div>
             </div>
         </main>
+        
+        <div><h1 class="mast"></h1></div>
 
-        <div class="feature-box-section">
-            <div class="box-container">
+        <div class="card-section">
+            <div class="container">
                 <div class="row">
-                    <div class="col-md-6 col-lg-4 text-white box-container">
-                        <div class="feature-box feat-one">
-                            <h3>{{ $featureBox1['title'] }}</h3>
-                            <p>{{ $featureBox1['text'] }}</p>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card text-center" style="width: 18rem;">
+                            <img class="card-img-top" src='/themes/wordplate/assets/images/specialrequests.png' alt="Taylor Bottls">
+                            <div class="card-body">
+                                <h5 class="card-title">Special Requests</h5>
+                                <p class="card-text">We are devoted to finding unique wine, spirits and beer to offer our most discriminating customers. But if you don't find what you're looking for, our team is always ready to assist, from tracking down one rare bottle to helping stock your bar for your next event.</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4 text-white box-container">
-                        <div class="feature-box feat-two">
-                            <h3>{{ $featureBox2['title'] }}</h3>
-                            <p>{{ $featureBox2['text'] }}</p>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card text-center" style="width: 18rem;">
+                            <img class="card-img-top" src='/themes/wordplate/assets/images/specialevents.png' alt="Wine Bottles">
+                            <div class="card-body">
+                                <h5 class="card-title">Special Event</h5>
+                                <p class="card-text">Need help selecting which wine, beer and spirits to serve at your special event? We'll make person- alized recommendations based on your taste, menu and size of your party. And with the largest inventory on The Forgotten Coast, there's a bottle to fit every budget.</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4 text-white box-container">
-                        <div class="feature-box feat-two">
-                            <h3>{{ $featureBox2['title'] }}</h3>
-                            <p>{{ $featureBox2['text'] }}</p>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card text-center" style="width: 18rem;">
+                            <img class="card-img-top" src='/themes/wordplate/assets/images/craftbeer.png' alt="Voodoo Ranger">
+                            <div class="card-body">
+                                <h5 class="card-title">Craft Beer</h5>
+                                <p class="card-text">Looking for craft beer? Then you're in luck! Port Fine Wine & Spirits offers domestic and imported beer and cider, and even low & non-al- coholic beer. Come check out our selection of stouts, pilsners, lagers, Belgian white ales, IPA's, and so much more!</p>
+                            </div>
                         </div>
                     </div>
                 </div>
